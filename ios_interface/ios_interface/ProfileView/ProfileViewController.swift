fa822ff08b178d9faa297dc2548ae90bc5bfa24d
Anton Kangin <Anton.kangin@gmail.com>
2021-06-22 21:13:01 +0400

    Profile storyboard added and initial

---
+++ ios_interface/ios_interface/ProfileView/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ios_interface
+//
+//  Created by Антон Кангин on 22.06.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
