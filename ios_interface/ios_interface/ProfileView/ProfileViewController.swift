160b9e78f33478db5fb185ad49523aee91ca7a7d
Anton Kangin <Anton.kangin@gmail.com>
2021-06-22 21:44:37 +0400

    Add ProfileView in Profile.storyboard

--- ios_interface/ios_interface/ProfileView/ProfileViewController.swift
+++ ios_interface/ios_interface/ProfileView/ProfileViewController.swift
@@ -8,13 +8,18 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-
+    let profileViewName = String(describing: ProfileView.self)
+    
+    if let profileView = Bundle.main.loadNibNamed(profileViewName, owner: nil, options: nil)?.first as? ProfileView {
+      profileView.frame = CGRect(x: 15, y: 15, width: view.bounds.width - 30, height: 500)
+      view.addSubview(profileView)
+      
+      profileView.cityLabel.text = "Togliatty"
+      profileView.nameLabel.text = "Anton"
+    }
+  }
 }
