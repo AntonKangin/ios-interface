531630f67fe69fa253412ccc1528246122d5e03c
Anton Kangin <Anton.kangin@gmail.com>
2021-06-22 21:02:50 +0400

    ProfileView files added

---
+++ ios_interface/ios_interface/ProfileView/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  ios_interface
+//
+//  Created by Антон Кангин on 22.06.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+  @IBOutlet weak var avatarImageView: UIImageView!
+  
+  @IBOutlet weak var nameLabel: UILabel!
+  
+  @IBOutlet weak var birthdayLabel: UILabel!
+  
+  @IBOutlet weak var cityLabel: UILabel!
+  
+  @IBOutlet weak var descriptionTextView: UITextView!
+}
